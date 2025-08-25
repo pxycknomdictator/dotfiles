7b5883d7a4704c7241fa8d6192a1b6d2329ff24e
Noman <pxycknomdictator@outlook.com>
2025-08-25 12:39:08 +0500

    add neovim configurations in dotfiles

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,26 @@
+-- Line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Indentation
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+
+-- Search
+vim.opt.ignorecase = true   
+vim.opt.smartcase = false  
+vim.opt.incsearch = true 
+
+-- UI tweaks
+vim.opt.cursorline = true
+vim.opt.termguicolors = true
+vim.opt.signcolumn = "yes"
+vim.opt.guicursor = ""           
+
+-- Scrolling
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
+
